97c192aaf7d05f7f74b58673cbaa1a3259f4dd1a
tredfort <d.rinat703@gmail.com>
2021-05-11 22:24:57 +0300

    new norm

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,29 +6,31 @@
 /*   By: tredfort <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 18:33:19 by tredfort          #+#    #+#             */
-/*   Updated: 2020/11/24 17:52:11 by tredfort         ###   ########.fr       */
+/*   Updated: 2021/04/19 17:33:23 by tredfort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "../includes/minishell.h"
+#define BUFFER_SIZE 42
 
-static char	*next_line(char *tmp_buf, char **line)
+static char	*residue_handling(char *residue, char **line)
 {
 	char	*ptr_n;
 
 	ptr_n = NULL;
-	if (tmp_buf)
+	if (residue)
 	{
-		if ((ptr_n = ft_strchr(tmp_buf, '\n')))
+		ptr_n = ft_strchr(residue, '\n');
+		if (ptr_n)
 		{
 			*ptr_n++ = '\0';
-			*line = ft_strdup(tmp_buf);
-			ft_strcpy(tmp_buf, ptr_n);
+			*line = ft_strdup(residue);
+			ft_strcpy(residue, ptr_n);
 		}
 		else
 		{
-			*line = ft_strdup(tmp_buf);
-			free(tmp_buf);
+			*line = ft_strdup(residue);
+			free(residue);
 		}
 	}
 	else
@@ -36,35 +38,31 @@ static char	*next_line(char *tmp_buf, char **line)
 	return (ptr_n);
 }
 
-int			get_next_line(int fd, char **line)
+int	get_next_line(int fd, char **line)
 {
-	static char	*tmp_buf;
-	char		*buf;
+	static char	*residue = NULL;
+	char		buf[BUFFER_SIZE + 1];
 	char		*ptr_n;
 	size_t		rs;
 
-	if (fd < 0 || !line || BUFFER_SIZE < 1 || !(buf = malloc(sizeof(char) *
-                                                             (BUFFER_SIZE + 1))))
+	if (fd < 0 || read(fd, buf, 0) < 0)
 		return (-1);
-	if (read(fd, buf, 0) < 0)
-    {
-	    free(buf);
-        return (-1);
-    }
-	ptr_n = next_line(tmp_buf, line);
-	while (ptr_n == NULL && (rs = read(fd, buf, BUFFER_SIZE)) > 0)
+	ptr_n = residue_handling(residue, line);
+	rs = 1;
+	while (!ptr_n && rs > 0)
 	{
+		rs = read(fd, buf, BUFFER_SIZE);
 		buf[rs] = '\0';
-		if ((ptr_n = ft_strchr(buf, '\n')))
+		ptr_n = ft_strchr(buf, '\n');
+		if (ptr_n)
 		{
 			*ptr_n++ = '\0';
-			tmp_buf = ft_strdup(ptr_n);
+			residue = ft_strdup(ptr_n);
 		}
-		*line = ft_strjoin(line, buf);
+		*line = ft_strjoin_and_free(line, buf);
 	}
-	free(buf);
 	if (ptr_n)
 		return (1);
-	tmp_buf = NULL;
+	residue = NULL;
 	return (0);
 }
