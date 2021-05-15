eafb88087740a3bba0917caf7b0644ce98fc1c81
tredfort <d.rinat703@gmail.com>
2021-05-15 22:38:09 +0300

    added function ft_free_array

--- libft/libft.h
+++ libft/libft.h
@@ -63,6 +63,7 @@ t_list				*ft_lstlast(t_list *lst);
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
 						void (*del)(void *));
 t_list				*ft_lstnew(void *content);
+void				ft_free_array(char **arr);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstclear(t_list **lst, void (*del)(void*));
